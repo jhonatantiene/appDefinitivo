36b20e9f66c9fa39bcf6ddbc20c9429758c3fb5b
jhonatantiene <91148527+jhonatantiene@users.noreply.github.com>
2023-08-21 10:41:00 -0300

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor() {
 
+  }
+
+  dadosPessoa = {
+    consumo: 0,
+    peso: 0,
+    totalAbeber: 0
+  }
+
+  graficoAnterior: any
+
+  ngOnInit(): void {
+
+  }
+
+  async grafico() {
+
+    if (this.dadosPessoa.peso > 0) {
+      const data = [
+        { aguaConsumida: this.dadosPessoa.consumo, count: this.dadosPessoa.consumo, msg: 'Agua consumida' },
+        { aguaRestante: this.dadosPessoa.totalAbeber - this.dadosPessoa.consumo, count: this.dadosPessoa.totalAbeber - this.dadosPessoa.consumo, msg: 'Restante' },
+      ];
+
+
+      if (this.graficoAnterior) {
+        this.graficoAnterior.destroy()
+      }
+
+      this.graficoAnterior = new Chart(
+        document.getElementById('graficos') as HTMLCanvasElement,
+        {
+          type: 'pie',
+          data: {
+            labels: ['Cosumiu', 'Restante'],
+            datasets: [
+              {
+                data: data.map(row => { return row.count })
+              }
+            ]
+          }
+        }
+      )
+    }
+  }
+
+  getDados(peso: number, aguaBebida: number) {
+    this.dadosPessoa.peso = peso
+    this.dadosPessoa.consumo = aguaBebida
+    this.dadosPessoa.totalAbeber = peso * 35 / 1000
+    this.grafico()
+  }
 }
